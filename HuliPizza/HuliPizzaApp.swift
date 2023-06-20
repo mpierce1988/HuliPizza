316d2dd6b41e1a6e73dd79767afd266a08dab1ab
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-20 13:15:23 -0300

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Michael Pierce on 2023-06-20.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
