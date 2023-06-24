7003b6195b94498ef57923004e9e513bad4f9d5b
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-24 17:55:21 -0300

    State variables and buttons

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var orders: [Int] = [1, 2, 3, 4, 6]
     
-    var showOrders: Bool = true
+    @State private var showOrders: Bool = true
     
     var body: some View {
         VStack {            
             HeaderView()
                 .shadow(radius: 5)
+            
+            Button {
+                showOrders.toggle()
+            } label: {
+                Image(systemName: showOrders ? "cart" : "menucard")
+                    .font(.title2)
+            }
+            .foregroundStyle(.white)
+            
             if(showOrders){
                 OrderView(orders: orders)
                     .cornerRadius(10)
