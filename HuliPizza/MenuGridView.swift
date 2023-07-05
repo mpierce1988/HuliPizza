d9072af575de7fa07fafcbe5cea25cb9673e4b7a
Michael Pierce <michael.todd.pierce@gmail.com>
2023-07-05 15:49:22 -0300

    Favorites Grid

--- HuliPizza/MenuGridView.swift
+++ HuliPizza/MenuGridView.swift
@@ -9,20 +9,46 @@ import SwiftUI
 
 struct MenuGridView: View {
     var menu: [MenuItem]
+    
+    @State private var favorites: [Int] = [-1]
+    
+    func menu(id:Int) -> MenuItem {
+        menu.first(where: {$0.id == id}) ?? noMenuItem
+    }
+    
     @State var selectedItem:MenuItem = noMenuItem
+   
+    @State private var favouritedItems: [MenuItem] = []
     // Three column layout of griditems
     let columnLayout = Array(repeating: GridItem(), count: 3)
+    let favouritesColumnLayout = Array(repeating: GridItem(), count: 5)
+  
     
     var body: some View {
+        
         VStack {
-            Text(selectedItem.name)
+            LazyVGrid(columns: favouritesColumnLayout) {
+                ForEach(favorites.sorted(), id: \.self) { item in
+                    FavoriteTileView(menuItem: menu(id: item))
+                        .onLongPressGesture {
+                            if let index = favorites.firstIndex(where: {$0 == item}) {
+                                favorites.remove(at: index)
+                            }
+                        }
+                }
+            }
+            Text("\(selectedItem.name)")
             ScrollView {
                 LazyVGrid(columns: columnLayout) {
                     ForEach(menu) {item in
-                        MenuItemTileView(menuItem: item)
-                            .onTapGesture {
-                                selectedItem = item
-                            }
+                        if !favorites.contains(item.id) {
+                            MenuItemTileView(menuItem: item)
+                                .onTapGesture(count: 2) {
+                                    if !favorites.contains(item.id) {
+                                        favorites.append(item.id)
+                                    }
+                                }
+                        }
                     }
                 }
             }
